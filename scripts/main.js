ad4d8312e8d782bd7fbdf0078b640d47c2f90fe4
binfang <1712643592@qq.com>
2023-02-24 11:35:51 +0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+function test_a(num1, num2) {
+    return num1 * num2;
+}
+
+document.querySelector("h1").addEventListener("click", function () {
+    console.log("跳转到百度");
+  });
